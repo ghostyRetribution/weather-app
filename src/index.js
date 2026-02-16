7f829fd0d54f9b250c2555038d0dd62d14f124f6
ghostyRetribution <213485414+ghostyRetribution@users.noreply.github.com>
2026-02-16 23:14:32 +0600

    project done

--- src/index.js
+++ src/index.js
@@ -3,30 +3,27 @@ import './styles.css';
 const apiKey = 'QZT3RJNA76KZFXGR896VM5CFJ';
 const input = document.querySelector('input');
 
-function main() {
-	if (input.value !== '') {
-		const city = input.value;
-		fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=${apiKey}&contentType=json`)
-			.then((response) => {
-				return response.json();
-			})
-			.then((response) => {
-				document.getElementById('weather-condition').textContent = response.currentConditions.conditions;
-				document.getElementById('city').textContent = city.toUpperCase();
-				document.getElementById('temperature').innerHTML = `${response.currentConditions.temp}<p>&degC</p>`;
-				document.getElementById('feel').textContent = `Feels like: ${response.currentConditions.feelslike}°C`;
-				document.getElementById('wind').textContent = `Wind: ${response.currentConditions.windspeed} Km/h`;
-				document.getElementById('humidity').textContent = `Humidity: ${response.currentConditions.humidity}%`;
-			})
-			.catch(() => {
-				const errorDisplay = document.createElement('div');
-				errorDisplay.classList.add('error-display');
-				errorDisplay.textContent = 'ERROR Please check your internet connection and search a valid city';
-				document.querySelector('body').appendChild(errorDisplay);
-				errorDisplay.addEventListener('click', () => {
-					errorDisplay.style.display = 'none';
-				});
-			});
+async function main() {
+	try {
+		if (input.value !== '') {
+			const city = input.value;
+			const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=${apiKey}&contentType=json`);
+			const data = await response.json();
+			document.getElementById('weather-condition').textContent = data.currentConditions.conditions;
+			document.getElementById('city').textContent = city.toUpperCase();
+			document.getElementById('temperature').innerHTML = `${data.currentConditions.temp}<p>&degC</p>`;
+			document.getElementById('feel').textContent = `Feels like: ${data.currentConditions.feelslike}°C`;
+			document.getElementById('wind').textContent = `Wind: ${data.currentConditions.windspeed} Km/h`;
+			document.getElementById('humidity').textContent = `Humidity: ${data.currentConditions.humidity}%`;
+		}
+	} catch {
+		const errorDisplay = document.createElement('div');
+		errorDisplay.classList.add('error-display');
+		errorDisplay.textContent = 'ERROR Please check your internet connection and search a valid city';
+		document.querySelector('body').appendChild(errorDisplay);
+		errorDisplay.addEventListener('click', () => {
+			errorDisplay.style.display = 'none';
+		});
 	}
 }
 
